b36e8b86c7e77b2c1aa109e83f4206790dfe9dd9
Lakshman Singh <lslakshman2024@gmail.com>
2025-05-30 17:14:05 +0530

    permation done

---
+++ DSA/Coding-patterns/Backtracking/Medium/Permutation.java
@@ -0,0 +1,38 @@
+package Medium;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Permutation {
+
+    public static void swap(int arr[], int i, int j) {
+        if (i == j)
+            return;
+        arr[i] = arr[i] + arr[j];
+        arr[j] = arr[i] - arr[j];
+        arr[i] = arr[i] - arr[j];
+    }
+
+    public static void permutation(int arr[], int index, List<List<Integer>> lists) {
+
+        if (index == arr.length) {
+            System.out.println(Arrays.toString(arr));
+            lists.add(IntStream.of(arr).boxed().collect(Collectors.toList()));
+            return;
+        }
+
+        for (int i = index; i < arr.length; i++) {            
+            swap(arr, i, index);
+            permutation(arr, index + 1, lists);
+            swap(arr, i, index);
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = new int[] { 1, 2, 3 };
+        List<List<Integer>> lists = new ArrayList<>();
+        permutation(arr, 0, lists);
+        System.out.println(lists);
+    }
+}
--- DSA/Coding-patterns/Backtracking/Medium/QuickSort.java
+++ DSA/Coding-patterns/Backtracking/Medium/QuickSort.java
@@ -1,1 +1,45 @@
+package Medium;
 
+import java.util.*;
+
+public class QuickSort {
+  // 1 6 7 8 9 2 3 0 5
+  // 1 0 7 8 9 2 3 6 5
+  public static void swap(int arr[], int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
+
+  public static int partition(int arr[], int low, int high) {
+    // { 1, 2, 0, 5, 4 }
+    // { 4, 2, 0, 5, 1 }
+    int s = low;
+    int e = high - 1;
+    int pivot = arr[low];
+    while (s < e) {
+      while (arr[s] <= pivot && s <= high - 1)
+        s++;
+      while (arr[e] > pivot && e >= low + 1)
+        e--;
+      if (s < e)
+        swap(arr, s, e);
+    }
+    swap(arr, e, low);
+    return e;
+  }
+
+  public static void quickSort(int arr[], int s, int e) {
+    if (s < e) {
+      int p = partition(arr, s, e);
+      quickSort(arr, s, p - 1);
+      quickSort(arr, p + 1, e);
+    }
+  }
+
+  public static void main(String[] a) {
+    int arr[] = new int[] { 1, 2, 4, 5, 4 };
+    quickSort(arr, 0, arr.length);
+    System.out.println(Arrays.toString(arr));
+  }
+}
