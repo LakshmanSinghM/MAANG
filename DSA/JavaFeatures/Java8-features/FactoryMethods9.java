8b4419a78e869d9e2a512bb29cfba9f06b147029
Lakshman Singh <lslakshman2024@gmail.com>
2025-06-04 20:46:39 +0530

    cleaned the class files

--- DSA/JavaFeatures/Java8-features/FactoryMethods9.java
+++ DSA/JavaFeatures/Java8-features/FactoryMethods9.java
@@ -7,9 +7,8 @@ public class FactoryMethods9{
         Set<Integer> set = Set.of(1,2,3,4);
         Map<Integer,String> map = Map.of(1,"one",2,"two");
         
-        System.out.println(Stream.of(1, 2, 3, 4, 5)
-                        .reduce(0, (x, y) -> x + y));
-        // String name = STR."""Lakshman"""; // java21
+        System.out.println(Stream.of(1, 2, 3, 4, 5).reduce(0, (x, y) -> x + y));
+        // String name = STR."""Lakshman"""; //java21
       //   String formattedName = STR."Hello, \{name} !";//java21
         System.out.println("formattedName");
        }
