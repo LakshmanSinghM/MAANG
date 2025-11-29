37ba805667e3274e7c7e1e01f242f49b8d428b3e
lakshmansinghmalviya <lslakshman2024@gmail.com>
2025-11-29 22:37:52 +0530

    Done with the nth root of the number

---
+++ DSA/Coding-patterns/binary-search/medium/FindNthRoot.java
@@ -0,0 +1,33 @@
+public class FindNthRoot {
+    static int mutiplyNTimes(int n, int k) {
+        int ans = k;
+        for (int i = 1; i<n; i++)
+            ans *= k;
+        System.out.println("Multip by time of the "+k+"    "+ans);    
+        return ans;
+    }
+
+    static int findNthRoot(int num, int n) {
+        int low = 1;
+        int high = num;
+        while (low <= high) {
+            int mid = (high + low) / 2;
+            int findNthSq = mutiplyNTimes(n, mid);
+
+            System.out.println("The nth multiple times value " + findNthSq+"     "+mid +"     "+n);
+            
+            if (num == findNthSq)
+                return mid;
+
+            if (findNthSq > num)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findNthRoot(69, 4));
+    }
+}
