523d63f444a999f3140650332fffd8368b2f5061
lakshmansinghmalviya <lslakshman2024@gmail.com>
2025-12-09 02:08:18 +0530

    in progress

---
+++ DSA/Coding-patterns/strings/AtoI.java
@@ -0,0 +1,38 @@
+public class AtoI {
+    
+    public static int myAtoi(String s) {
+        s = s.trim();
+        int sign = 1;
+        long num = 0;
+
+        if (s.startsWith("-")) {
+            sign = -1;
+            s = s.substring(1);
+        } else if (s.startsWith("+")) {
+            s = s.substring(1);
+            sign = 1;
+        }
+
+        if (s.isEmpty())
+            return 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (!Character.isDigit(s.charAt(i)))
+                break;
+
+            num = num * 10 + (s.charAt(i) - '0');
+
+            if (sign * num >= Integer.MAX_VALUE)
+                return Integer.MAX_VALUE;
+
+            if (sign * num <= Integer.MIN_VALUE)
+                return Integer.MIN_VALUE;
+        }
+        return (int) num * sign;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myAtoi("-+12"));
+        System.out.println(myAtoi("-12"));
+    }
+}
---
+++ DSA/Coding-patterns/strings/CountNumberSubstr.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+
+public class CountNumberSubstr {
+
+    // private static int countNumberSubstr(String s, int k) {
+    // int cnt = 0;
+
+    // for (int i = 0; i < s.length(); i++) {
+    // Set<Character> set = new HashSet<>();
+
+    // for (int j = i; j < s.length(); j++) {
+    // set.add(s.charAt(j));
+    // if (set.size() == k)
+    // cnt++;
+    // if (set.size() > k)
+    // break;
+    // }
+    // }
+    // return cnt;
+    // }
+
+    private static int countNumberSubstr(String s, int k) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int cnt = 0, l = 0, r = 0;
+
+        for (r = 0; r < s.length(); r++) {
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
+
+            if (map.size() > k) {
+                map.put(s.charAt(l), map.getOrDefault(s.charAt(l), 0) - 1);
+
+                if (map.getOrDefault(s.charAt(l), 0) == 0)
+                    map.remove(s.charAt(l));
+                l++;
+            }
+
+            cnt = cnt + (r - l + 1);
+        }
+        return cnt;
+    }
+
+    private static int countNumberSubstring(String s, int k) {
+        // this is the formula to get the total
+        // pending to test
+        return countNumberSubstr(s, k) - countNumberSubstr(s, k - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countNumberSubstring("pqpqs", 2));
+    }
+}
