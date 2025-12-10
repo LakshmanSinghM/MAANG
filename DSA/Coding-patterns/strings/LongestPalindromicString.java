daed86322d123f6e8be4931cf8711a6121e1a616
lakshmansinghmalviya <lslakshman2024@gmail.com>
2025-12-11 01:55:55 +0530

    done with the string medium problems

---
+++ DSA/Coding-patterns/strings/LongestPalindromicString.java
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/longest-palindromic-substring/
+
+public class LongestPalindromicString {
+
+    static String palindrom(int i, int j, String s) {
+        // a
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            i--;
+            j++;
+        }
+        return s.substring(i + 1, j);
+    };
+
+    static String longestPalindromString(String s) {
+        int max = 0;
+        String str = "";
+        for (int i = 0; i < s.length(); i++) {
+            String even = palindrom(i, i + 1, s);
+            if (max < even.length()) {
+                max = even.length();
+                str = even;
+            }
+            String odd = palindrom(i, i, s);
+            if (max < odd.length()) {
+                max = odd.length();
+                str = odd;
+            }
+        }
+        return str;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindromString("abba"));
+    }
+}
--- DSA/JavaFeatures/Java8-features/joda-api/FindAge.java
+++ DSA/JavaFeatures/Java8-features/joda-api/FindAge.java
@@ -4,6 +4,6 @@ public class FindAge {
     public static void main(String[] args) {
         LocalDate date = new LocalDate();
         Period p= date.between("10-06-2003",date);
-        System.out.println("The age is "+p.);
+        System.out.println("The age is "+p.getYears()+"  "+p.getDays()+"  "+p.getMonths()+"  "+p.getChronology());
     }
 }
