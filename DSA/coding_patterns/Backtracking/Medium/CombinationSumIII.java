e50542cf20cdb09b005cde4202d7a5e10b562d48
lakshmansinghmalviya <lslakshman2024@gmail.com>
2025-12-26 02:03:10 +0530

    done with the combination sum3

---
+++ DSA/coding_patterns/Backtracking/Medium/CombinationSumIII.java
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/combination-sum-iii/description/
+
+package medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSumIII {
+
+    void listAllComboOfKSizeWithKSum(List<List<Integer>> lists, int start, int n, int k, int target,
+            List<Integer> list) {
+        if (target < 0 || start >= n)
+            return;
+
+        if (list.size() == k && target == 0) {
+            lists.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int index = start; index <=n; index++) {
+            list.add(index);
+            listAllComboOfKSizeWithKSum(lists, index + 1, n, k, target - index, list);
+            list.removeLast(); 
+        }
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> lists = new ArrayList<>();
+        int k = 3;
+        int target = 10;
+        new CombinationSumIII().listAllComboOfKSizeWithKSum(lists, 1, 9, k, target, new ArrayList<>());
+        System.out.println(lists);
+    }
+}
--- DSA/coding_patterns/Backtracking/Medium/SubsetsII.java
+++ DSA/coding_patterns/Backtracking/Medium/SubsetsII.java
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/subsets-ii/description/
+
 package medium;
 
 import java.util.*;
