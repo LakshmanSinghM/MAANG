4b7fb0ef52c26322f36676e94bba8234340261e1
lakshmansinghmalviya <lslakshman2024@gmail.com>
2026-01-02 01:51:13 +0530

    Done with the devide by bit

--- DSA/coding_patterns/bit_manipulation/basic/DivideToNumWithoutDMOperator.java
+++ DSA/coding_patterns/bit_manipulation/basic/DivideToNumWithoutDMOperator.java
@@ -5,41 +5,32 @@ package coding_patterns.bit_manipulation.basic;
 public class DivideToNumWithoutDMOperator {
 
     int divideTwoNumberWithoutUsingMulAndDevideOperator(int dividend, int divisor) {
-
         if (dividend == divisor)
             return 1;
 
         int sign = 1;
+
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+
         if ((dividend > 0 && divisor < 0) || (divisor > 0 && dividend < 0))
             sign = -1;
 
-        int num = Math.abs(dividend);
-        int d = Math.abs(divisor);
+        long num = Math.abs((long) dividend);
+        long d = Math.abs((long) divisor);
         int cnt = 0;
-        int ans = 0;
-
-        // needs some clarification in this solution
-        // not solved yet fully
-        System.out.println(1 << 2);
+        long ans = 0;
 
         while (num >= d) {
 
             cnt = 0;
             while (num >= (d << (cnt + 1))) {
-                System.out.println(d << (cnt + 1));
                 cnt++;
             }
             ans = ans + (1 << cnt);
             num = num - (d << cnt);
         }
-
-        if (ans == Integer.MAX_VALUE && sign == 1) {
-            return Integer.MAX_VALUE;
-        }
-        if (ans == Integer.MAX_VALUE && sign == -1) {
-            return Integer.MIN_VALUE;
-        }
-        return ans;
+        return sign < 0 ? (int) ans * -1 : (int) ans;
     }
 
     public static void main(String[] args) {
