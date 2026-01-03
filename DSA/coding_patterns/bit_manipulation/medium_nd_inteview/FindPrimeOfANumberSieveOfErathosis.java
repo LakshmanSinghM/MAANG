cd895c40d4ded71c3d68a7f6af63ac22d7e0dc03
lakshmansinghmalviya <lslakshman2024@gmail.com>
2026-01-04 00:20:51 +0530

    Done with the seive of the erathosis

---
+++ DSA/coding_patterns/bit_manipulation/medium_nd_inteview/FindPrimeOfANumberSieveOfErathosis.java
@@ -0,0 +1,86 @@
+// https://takeuforward.org/plus/dsa/problems/count-primes-in-range-l-to-r
+
+package coding_patterns.bit_manipulation.medium_nd_inteview;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindPrimeOfANumberSieveOfErathosis {
+
+    int[] preComputeBlackBox(int n) {
+        int arr[] = new int[n + 1];
+        Arrays.fill(arr, 1);
+
+        for (int i = 2; i * i <= n; i++) {
+            for (int j = i * i; j <= n; j = j + i) {
+                arr[j] = 0;
+            }
+        }
+        return arr;
+    }
+
+    int countPrimeinRange(int arr[], int left, int right) {
+        int cnt = 0;
+        if (left < 2) {
+            left = 2;
+        }
+        if (right<2) {
+            right = 2;
+        }
+        for (int i = left; i <= right; i++) {
+            if (arr[i] == 1)
+                cnt++;
+        }
+        return cnt;
+    }
+
+    List<Integer> findPrimesBetbeenRange(int arr[][]) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int left = arr[i][0];
+            int right = arr[i][1];
+            int computedArray[] = preComputeBlackBox(right);
+            int primeCount = countPrimeinRange(computedArray, left, right);
+            list.add(primeCount);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        // int arr[][] = { { 2, 5 }, { 4, 7 } };
+        int arr[][] = { { 1, 7 }, { 3, 7 } };
+        FindPrimeOfANumberSieveOfErathosis findPrimeOfANumberSieveOfErathosis = new FindPrimeOfANumberSieveOfErathosis();
+
+        System.out.println(findPrimeOfANumberSieveOfErathosis.findPrimesBetbeenRange(arr));
+    }
+}
+
+// 2 × 2
+// 2 × 3
+// 2 × 4
+// 2 × 5
+// .
+// .
+
+// 3 × 2
+// 3 × 3
+// 3 × 4
+// 3 × 5
+// .
+// .
+
+// 5 × 2
+// 5 × 3
+// 5 × 4
+// 5 × 5
+
+// 7 × 2 ✓
+// 7 × 3 ✓
+// 7 × 4 ✓
+// 7 × 5 ✓
+// 7 × 6 ✓
+// 7 × 7
+
+// = 42
